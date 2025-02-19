bf189a9f3eed95a9cb8ea32a94f86fcdc0cad834
Barth <barthelemy@azmed.co>
2025-02-19 16:30:19 +0100

    darker version

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,50 @@
 import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, CircleMarker, Popup } from 'react-leaflet';
 import L from 'leaflet';
-import icon from 'leaflet/dist/images/marker-icon.png';
-import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 import pointsData from './locations.json';
 
-let DefaultIcon = L.icon({
-  iconUrl: icon,
-  shadowUrl: iconShadow,
-  iconSize: [25, 41],
-  iconAnchor: [12, 41]
-});
-
-L.Marker.prototype.options.icon = DefaultIcon;
+// Define the bounds for Europe
+const europeBounds = L.latLngBounds(
+  L.latLng(35, -10), // Southwest corner 
+  L.latLng(72, 30)   // Northeast corner 
+);
 
 function App() {
   return (
-    <MapContainer center={[0, 0]} zoom={2} style={{ height: '400px', width: '800px' }}>
+    <MapContainer 
+      center={[50, 10]} 
+      zoom={3} 
+      style={{ height: '500px', width: '500px' }}
+      maxBounds={europeBounds}
+      maxBoundsViscosity={1.0}
+    >
       <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+        attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+        maxZoom={20}
       />
       {pointsData.points.map((point) => (
-        <Marker key={point.id} position={[point.latitude, point.longitude]}>
+        <CircleMarker
+          key={point.id}
+          center={[point.latitude, point.longitude]}
+          radius={3}  // Adjust the radius as needed
+          color="red"  // Set the color to red
+          fillColor="red"
+          fillOpacity={1}
+        >
           <Popup>
+            <h3>{point.name}</h3>
+            <p>{point.description}</p>
             <img 
               src={point.logo} 
               alt={`${point.name} logo`} 
               style={{ width: '100px', height: '100px', objectFit: 'cover', marginTop: '10px' }} 
             />
-            <h3>{point.name}</h3>
-            <p>{point.description}</p>
             <p>
               <a href={point.link} target="_blank" rel="noopener noreferrer">Visit Website</a>
             </p>
           </Popup>
-        </Marker>
+        </CircleMarker>
       ))}
     </MapContainer>
   );
